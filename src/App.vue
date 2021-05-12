d5091db00839ba15641a34625da037cf120275a3
Ferdiansyah <ferdif9996@gmail.com>
2021-05-13 05:17:16 +0700

    update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,20 @@
 <template>
-  <div class="bg-gray-200 w-full sm:w-3/5 md:w-2/5 mx-auto h-screen relative">
-    <nav class="bg-blue-700 flex p-2 h-auto absolute top-0 left-0 w-full shadow-md">
-      <div class="flex flex-wrap w-1/2 justify-start">
-        <h5 class="text-white p-2 font-bold">App Chat</h5>
-      </div>
-      <div class="flex flex-wrap w-1/2 justify-end mr-2">
-        <MenuIcon class="h-10 text-white float-right cursor-pointer" />
-      </div>
-    </nav>
-    <div class="overflow-auto h-screen">
+  <div class="bg-gray-200 w-full mx-auto">
+    <Navbar/>
+    <div class="bg-blue-100 mt-20">
       <router-view/>
     </div>
   </div>
 </template>
 <script>
-import {MenuIcon} from '@vue-hero-icons/outline'
+import Navbar from '@/components/Navbar.vue'
 export default{
   name: 'App',
   components:{
-    MenuIcon
+    Navbar
   },
   mounted(){
-    document.body.classList.add('overflow-hidden', 'bg-black')
+    document.body.classList.add('bg-blue-100')
   }
 }
 </script>
